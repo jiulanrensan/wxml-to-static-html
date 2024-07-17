d9063fa99cd4ba0116e47da130b73d1097e45e18
lijiacheng <839492148@qq.com>
2024-07-17 19:43:59 +0800

    wip: css parser demo

---
+++ src/css/index.js
@@ -0,0 +1,63 @@
+const { wxmlToHtmlInCss } = require('../const')
+const { readFile } = require('../fileHandler')
+const cssParser = require('css')
+const { rpx2px } = require('./utils')
+const { simulateDeviceInfo } = require('../config')
+
+/**
+ * image => img
+ * rpx => px
+ * import
+ * Page => body
+ */
+
+function tagTransform(selectors) {
+  return selectors.map((se) => wxmlToHtmlInCss[se])
+}
+
+function rpxTransform(declaretion) {
+  const { value } = declaretion
+  if (!value || !value.includes('rpx')) return
+  const rpxValue = value.split('rpx')[0]
+  Object.assign(declaretion, {
+    value: `${rpx2px(rpxValue, simulateDeviceInfo)}px`,
+  })
+}
+
+function transform(stylesheet) {
+  stylesheet.rules.forEach((rule) => {
+    if (rule.type === 'rule') {
+      // declarations: css key-value
+      // selectors: class name
+      const { declarations, selectors } = rule
+      rule.selectors = tagTransform(selectors)
+      declarations.forEach((de) => {
+        // property: css key
+        // value: css value
+        /**
+         * @typedef Declaration
+         * @property {string} Declaration.property
+         * @property {string} Declaration.value
+         */
+        const { property, value } = de
+        // deal with css key,value
+        rpxTransform(de)
+      })
+    }
+  })
+}
+
+async function cssParse(filePath) {
+  try {
+    const cssStr = await readFile(filePath)
+    const ast = cssParser.parse(cssStr)
+    transform(ast.stylesheet)
+    console.log('ast', ast)
+  } catch (error) {
+    console.log('cssParse', error)
+  }
+}
+
+module.exports = {
+  cssParse,
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const { parse } = require('@wxml/parser')
 const { generate } = require('@wxml/generator')
 const { traverse } = require('@wxml/traverse')
+const path = require('path')
 const { tagMap, ElementType, ignoreKey } = require('./const')
 
 const { wxmlTagToHtmlTag, elementInterpolationToDefaultText, removeWxmlTag } = require('./wxml2html')
 const { readFile, writeFile } = require('./fileHandler')
+const { cssParse } = require('./css')
 
 function traverseAST(ast) {
   traverse(ast, {
@@ -20,11 +22,12 @@ function traverseAST(ast) {
 }
 
 async function main() {
-  const wxmlStr = await readFile('./static/input.wxml')
+  const wxmlStr = await readFile(path.resolve(__dirname, './static/input/index.wxml'))
   const ast = parse(wxmlStr)
   traverseAST(ast)
-  const code = generate(ast)
-  writeFile(code, './static/output.html')
+  // const code = generate(ast)
+  cssParse(path.resolve(__dirname, './static/input/index.wxss'))
+  // writeFile(code, './static/output.html')
 }
 
 main()
