bd981d6d07006d01bed3e86a6b7528c591652bba
20190225001 <839492148@qq.com>
2024-07-21 20:23:37 +0800

    feat: add css const

--- src/css/const.js
+++ src/css/const.js
@@ -1,12 +1,19 @@
 const { tagMap } = require('../wxml/const')
 
 const wxmlToHtmlInCss = {
+  ...tagMap,
   Page: 'body',
-  image: tagMap['image'],
-  text: tagMap['text'],
-  view: tagMap['view'],
 }
+
+const wxmlTagKeys = Object.keys(wxmlToHtmlInCss)
+
+const wxmlTagRegMap = wxmlTagKeys.reduce((reg, key) => {
+  reg[key] = new RegExp(`^${wxmlToHtmlInCss[key]}$`)
+  return reg
+}, {})
 
 module.exports = {
   wxmlToHtmlInCss,
+  wxmlTagKeys,
+  wxmlTagRegMap,
 }
